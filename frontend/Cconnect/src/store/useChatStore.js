81b5de7bcf194bfedf95f047549f38eccb8e7739
chhavi <chhavi2544gautam@gmail.com>
2026-03-06 13:38:30 +0530

    send message functionality implemented

---
+++ frontend/Cconnect/src/store/useChatStore.js
@@ -0,0 +1,53 @@
+import {create} from "zustand";
+export const useChatStore=create((set,get)=>({
+  allContacts:[],
+  Chats:[],
+  messages:[],
+  activeTab:"chats",
+  selectedUser:null,
+  isUsersLoading:false,
+  isMessagesLoading:false,
+  isSoundEnabled:JSON.parse(localStorage.getItem("soundEnabled"))==="true" 
+  toggleSound:()=>{
+    localStorage.setItem("isSoundEnabled)");
+    set({isSoundEnabled:!get().isSoundEnabled});
+  },
+  setActiveTab:(tab)=>{set({activeTab:tab}),
+  setSelectedUser:(selectedUser)=>({selectedUser}),
+
+  getAllContacts:async()=>{
+    set({isUsersLoading:true});
+    try{
+      const res=await axiosInstance.get("/messages/contacts");
+      set({allContacts:res.data});
+    }catch(error){
+      Toast.error(error.response.data.message);
+    }finally{
+      set({isUsersLoading:false});
+    }
+  },
+  getMyChatPartners:async()=>{
+    set({isUsersLoading:true});
+    try{
+      const res=await axiosInstance.get("/messages/my-chats");
+      set({Chats:res.data});
+    }catch(error){
+      Toast.error(error.response.data.message);
+    }finally{
+      set({isUsersLoading:false});
+    }
+  },
+
+  getMessagesByUserId:async(userId)=>{
+
+    set({isMessagesLoading:true});
+    try{
+      const res=await axiosInstance.get(`/messages/${userId}`);
+      set({messages:res.data});
+    }catch(error){
+      Toast.error(error.response?.data?.message || "Something went wrong");
+    }finally{
+      set({isMessagesLoading:false});
+    }
+  },
+}));
