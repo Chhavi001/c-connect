bc3a5a004dcc841d604d5ab70e73dd6349b40cf7
chhavi <chhavi2544gautam@gmail.com>
2026-03-06 23:20:10 +0530

    ready for deployment

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,38 +1,21 @@
-import express from 'express';
 import cookieParser from "cookie-parser";
-
-import path from "path";
 import cors from "cors";
 import {ENV} from "./lib/env.js";
 import authRoutes from "./routes/auth.routes.js";
 import messageRoutes from "./routes/message.route.js";
 import { connectDB } from "./lib/db.js";
+import { app, server } from "./lib/socket.js";
 
-
-
-
-const app = express();
-const __dirname=path.resolve();
-const PORT =ENV.PORT || 3000;
-
+const PORT = ENV.PORT || 3000;
 
 app.use(express.json())  //req.body
-
+app.use(cookieParser());
 app.use(cors({origin:ENV.CLIENT_URL,credentials:true}));
 
-app.use("/api/auth",authRoutes)
+app.use("/api/auth",authRoutes);
 app.use("/api/messages",messageRoutes);
-app.use(cookieParser())
-//make ready for deployment
-if(ENV.NODE_ENV ==="production"){
-  app.use(express.static(path.join(__dirname,"../frontend/dist")))
 
-  app.get("*",(__,res)=>{
-    res.sendFile(path.join(__dirname,"../frontend/dist/index.html"))
-  })
-}
-
-app.listen(PORT, ()=>{
-  console.log("server running on port " + PORT)
+server.listen(PORT, ()=>{
+  console.log("server running on port " + PORT);
   connectDB();
 });
